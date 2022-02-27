bd3570569f5d3b10a0e52b3664c500e896f7375b
Vighnesh Raut <vighnesh.raut13@gmail.com>
2022-02-28 01:49:58 +0530

    refactor: move main component in a different file

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,13 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 
-import { bundle } from './bundler';
-import { debounce } from './debounce';
-import { CodeEditor } from './CodeEditor';
-import { CodePreview } from './CodePreview';
 import { Header } from './Header';
+import { Main } from './Main';
 
 function App() {
-  const [inputCode, setInputCode] = useState('');
-  const [outputCode, setOutputCode] = useState('');
-  const [bundling, setBundling] = useState(false);
-  const [error, setError] = useState('');
-
-  const debouncedBundle = useCallback(
-    debounce(async (inputCode: string) => {
-      const { outputCode, error } = await bundle(inputCode);
-      setOutputCode(outputCode);
-      setError(error);
-      setBundling(false);
-    }, 2000),
-    []
-  );
-
-  // compile the code on change
-  useEffect(() => {
-    setBundling(true);
-    setError('');
-    debouncedBundle(inputCode);
-  }, [debouncedBundle, inputCode]);
-
   return (
     <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
       <Header />
-      <main
-        className="container"
-        style={{
-          flexGrow: 1,
-          display: 'flex',
-          alignItems: 'stretch',
-          flexBasis: '100%',
-        }}
-      >
-        <CodeEditor onChange={setInputCode} />
-        <CodePreview outputCode={outputCode} />
-      </main>
+      <Main />
     </div>
   );
 }
---
+++ src/Main.tsx
@@ -0,0 +1,49 @@
+/**
+ * @author Vighnesh Raut <rvighnes@amazon.com>
+ */
+
+import React, { useCallback, useEffect, useState } from 'react';
+
+import { CodeEditor } from './CodeEditor';
+import { CodePreview } from './CodePreview';
+import { debounce } from './debounce';
+import { bundle } from './bundler';
+
+export function Main(): JSX.Element {
+  const [inputCode, setInputCode] = useState('');
+  const [outputCode, setOutputCode] = useState('');
+  const [bundling, setBundling] = useState(false);
+  const [error, setError] = useState('');
+
+  const debouncedBundle = useCallback(
+    debounce(async (inputCode: string) => {
+      const { outputCode, error } = await bundle(inputCode);
+      setOutputCode(outputCode);
+      setError(error);
+      setBundling(false);
+    }, 2000),
+    []
+  );
+
+  // compile the code on change
+  useEffect(() => {
+    setBundling(true);
+    setError('');
+    debouncedBundle(inputCode);
+  }, [debouncedBundle, inputCode]);
+
+  return (
+    <main
+      className="container"
+      style={{
+        flexGrow: 1,
+        display: 'flex',
+        alignItems: 'stretch',
+        flexBasis: '100%',
+      }}
+    >
+      <CodeEditor onChange={setInputCode} />
+      <CodePreview outputCode={outputCode} />
+    </main>
+  );
+}
